b264d30ba370cdb3aae976e3b559403eed387bfb
Emil <emil19@users.noreply.github.com>
2021-10-10 14:47:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='social-auth-liu',
-    version='0.0.3',
+    version='0.0.4',
     py_modules=['social_liu'],
-    url='https://github.com/ovidner/python-social-auth-liu',
+    url='https://github.com/Baljan/python-social-auth-liu',
     license='MIT',
     author='Olle Vidner',
     author_email='olle@vidner.se',
     description='',
     install_requires=[
-        'social-auth-core==1.*',
-        'PyJWT==1.*',
-        'cryptography==1.*',
+        'social-auth-core==4.*',
+        'PyJWT==2.*',
+        'cryptography==3.*',
     ]
 )
